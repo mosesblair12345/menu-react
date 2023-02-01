0f49755d776fe5b53cb235953a0adcec43709ea1
moses <mosesblair00@gmail.com>
2023-02-01 09:36:17 +0300

    menu react

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import React, { useState } from "react";
+import Menu from "./Menu";
+import Categories from "./Categories";
+import items from "./data";
+
+const allCategories = [
+  "all",
+  ...new Set(
+    items.map((item) => {
+      return item.category;
+    })
+  ),
+];
+
+function App() {
+  const [menuItems, setMenuItems] = useState(items);
+
+  const filterItems = (category) => {
+    if (category === "all") {
+      setMenuItems(items);
+      return;
+    }
+    const newItems = items.filter((items) => {
+      return items.category === category;
+    });
+    setMenuItems(newItems);
+  };
+  return (
+    <main>
+      <section className="menu">
+        <div className="title">
+          <h2>Our Menu</h2>
+          <div className="underline"></div>
+        </div>
+        <Categories filterItems={filterItems} categories={allCategories} />
+        <Menu items={menuItems} />
+      </section>
+    </main>
+  );
+}
+
+export default App;
---
+++ src/Menu.js
@@ -0,0 +1,22 @@
+const Menu = ({ items }) => {
+  return (
+    <div className="section-center">
+      {items.map(({ id, title, price, img, desc }) => {
+        return (
+          <article className="menu-item" key={id}>
+            <img className="photo" src={img} alt={title} />
+            <div className="item-info">
+              <header>
+                <h4>{title}</h4>
+                <h4 className="price">$ {price}</h4>
+              </header>
+              <p className="item-text">{desc}</p>
+            </div>
+          </article>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Menu;
